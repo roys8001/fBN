33d7937885041473f95da47a7db61cddafdf0b62
Saptarshi Roy <roys8001@tamu.edu>
2021-11-18 23:45:07 -0600

    Add a boolean function checkDAG to check whether a graph is DAG or not

--- src/fBN.cpp
+++ src/fBN.cpp
@@ -4,7 +4,6 @@
 using namespace arma;
 
 // [[Rcpp::export]]
-
 double rgammatr(double a, double b, vec range, double n = 1) {
   // obtain environment containing function
   Rcpp::Environment RGeode = Rcpp::Environment::namespace_env("RGeode");
@@ -14,3 +13,27 @@ double rgammatr(double a, double b, vec range, double n = 1) {
   // return object
   return Rcpp::as<double>(trgamma(n, a, b, range));
 }
+
+Rcpp::Function dgamma("dgamma");
+Rcpp::Function sample("sample");
+
+// [[Rcpp::export]]
+bool checkDAG(mat G) {
+  mat cG = G; bool isDAG = TRUE;
+
+  // find non-leaf nodes
+  uvec index = find(sum(cG, 0) != 0);
+  while(index.n_elem < cG.n_rows) {
+    // delete leaf nodes
+    cG = cG(index, index);
+    if(accu(cG) == 0) break; else {
+      // find remaining non-leaf nodes
+      index = find(sum(cG, 0) != 0);
+    }
+  }
+
+  if(accu(cG) != 0) isDAG = FALSE;
+
+  return(isDAG);
+}
+
