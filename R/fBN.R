a8888569aba941c3d8f7684e7dbfe2c186d5b5a8
Saptarshi Roy <roys8001@tamu.edu>
2021-11-20 23:01:20 -0600

    Add function to generate initial principal components

--- R/fBN.R
+++ R/fBN.R
@@ -80,3 +80,58 @@ get_initial_zcoef = function(xobs, zobs, sbasis, svar, fscore, fpc, time) {
   return(list(zcoef = zcoef, eta = eta))
 }
 
+### Function to generate initial principal components
+
+get_initial_scoef = function(xobs, zobs, zcoef, sbasis, penat, svar, fscore, fpc, time) {
+  utime = sort(unique(time)); numc = length(svar); numk = ncol(fscore) / numc; numx = nrow(xobs); nums = ncol(sbasis)
+  # get positions of nodes
+  cindex = c(1, which(time[-1] < time[-length(time)]) + 1, length(time) + 1); findex = seq(1, numc * numk + numc, by = numk)
+  # initialize smoothness parameter
+  lambda = rep(1e-8, numk)
+
+  scoef = matrix(0, nums, numk)
+  # recursively update for components
+  for(k in 1 : numk) {
+    bsum = Bsum = 0
+
+    for(j in 1 : numc) {
+      cposition = cindex[j] : (cindex[j + 1] - 1); tposition = match(time[cposition], utime); fposition = findex[j] : (findex[j + 1] - 1)
+      # sum over non-missing values
+      for(i in 1 : numx) {
+        nposition = which(!is.na(xobs[i, cposition])); ncposition = cposition[nposition]; ntposition = tposition[nposition]
+        # remove the baseline effects
+        yobs = xobs[i, ncposition, drop = FALSE] - crossprod(zcoef[j, ], kronecker(zobs[i, ], t(sbasis[ntposition, , drop = FALSE])))
+        # get contribution for mean
+        bsum = bsum + crossprod(sbasis[ntposition, , drop = FALSE], t(yobs - tcrossprod(fscore[i, fposition[-k]], fpc[[j]][nposition, -k, drop = FALSE])) / svar[j] * fscore[i, fposition[k]])
+        # get contribution from precision
+        Bsum = Bsum + fscore[i, fposition[k]] * fscore[i, fposition[k]] / svar[j] * crossprod(sbasis[ntposition, , drop = FALSE])
+      }
+    }
+
+    cholmat = chol(Bsum + diag(c(rep(1e-8, 2), rep(lambda[k], (nums - 2)))))
+    # calculate the unconstrained vector
+    ucont = backsolve(cholmat, forwardsolve(t(cholmat), bsum))
+    # use sequential orthogonality for initialization
+    if(k > 1){
+      ocont = crossprod(t(penat), scoef[, 1 : (k - 1)])
+      scont = backsolve(cholmat, forwardsolve(t(cholmat), ocont))
+      ucont = ucont - crossprod(t(scont), crossprod(t(chol2inv(chol(crossprod(ocont, scont)))), crossprod(ocont, ucont)))
+    }
+
+    # get the final estimate by re-normalization
+    unorm = sqrt(as.numeric(tcrossprod(crossprod(ucont, penat), t(ucont)))); scoef[, k] = ucont / unorm
+    fscore[, seq(from = k, to = numc * numk, by = numk)] = fscore[, seq(from = k, to = numc * numk, by = numk)] * unorm
+
+    # conditional maximum likelihood estimate of Lambda
+    lambda[k] = (numc - 2) / crossprod(scoef[- c(1, 2), k])
+
+    for(j in 1 : numc) {
+      tposition = match(time[cindex[j] : (cindex[j + 1] - 1)], utime)
+      fpc[[j]][, k] = crossprod(t(sbasis[tposition, ]), scoef[, k])
+    }
+  }
+
+  list(scoef = scoef, lambda = lambda, fscore = fscore)
+}
+
+
