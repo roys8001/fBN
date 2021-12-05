1f6432b4020d1e6c566077fbb81e2fc755ce0aa7
Saptarshi Roy <roys8001@tamu.edu>
2021-12-04 22:48:25 -0600

    Add a function to generate initial parameters.

--- R/fBN.R
+++ R/fBN.R
@@ -210,3 +210,99 @@ get_initial_scoef = function(xobs, zobs, zcoef, sbasis, penat, svar, fscore, fpc
   list(scoef = scoef, lambda = lambda, fscore = fscore)
 }
 
+### generate initial parameters ###
+get_initial_parameter = function(xobs, zobs, time, numk = NULL, nknots = 20, tolCPV = 0.9, useAT = FALSE) {
+  utime = sort(unique(time)); cindex = c(1, which(time[-1] < time[-length(time)]) + 1, length(time) + 1)
+  numc = length(cindex) - 1; numx = nrow(xobs)
+  # get relevant spline information
+  sinfo = get_sinfo(time, nknots); sbasis = sinfo$sbasis; penat = sinfo$penat
+
+  # specify the time points to interpolate for initialization using SVD
+  if(useAT) t = utime else t = c(sinfo$lknot, sinfo$iknot, sinfo$uknot)
+
+  # compute a complete matrix without missing values (combined all samples and nodes)
+  xc = array(0, c(numc * numx, length(t)))
+  # iterate over each node
+  for(j in 1 : numc) {
+    yobs = xobs[, cindex[j] : (cindex[j + 1] - 1)]; scase = which(rowSums(!is.na(yobs)) != 0)
+    # smooth across time if observation is partially missing
+    yobs[scase, ] = t(apply(yobs[scase, ], 1, function(x) splinefun(time[cindex[j] : (cindex[j + 1] - 1)], x, method = 'natural') (time[cindex[j] : (cindex[j + 1] - 1)])))
+    # smooth across sample if observation is completely missing
+    yobs = apply(yobs, 2, function(x) {splinefun(1 : numx, x, method = 'natural') (1 : numx)})
+
+    # xc is a stacked data matrix with numc time numx rows and |t| columns
+    yobs = t(apply(yobs, 1, function(x) splinefun(time[cindex[j] : (cindex[j + 1] - 1)], x, method = 'natural') (t)))
+    # remove the baseline effect
+    xc[(1 : numx) + (j * numx - numx), ] = apply(yobs, 2, function(x) lm(x ~ zobs)$residual)
+  }
+
+  # compute SVD of the imputed data matrix and cumulative sums of the squares proportions
+  svdx = svd(xc); cumv = cumsum(svdx$d^2 / sum(svdx$d^2))
+  # select number of principal components based on cumulative sums if numk is unspecified
+  if(is.null(numk)) numk = max(2, which(cumv >= tolCPV)[1])
+
+  print(t(matrix(cumv, dimnames = list(paste('numk =', 1 : length(svdx$d))))))
+
+  # get initial principal components and scores
+  findex = seq(1, numc * numk + numc, by = numk); fpc = vector("list", numc); fscore = matrix(0, numx, numc * numk)
+  # iterate over all nodes
+  for(j in 1 : numc) {
+    # if use all time points then subset for each node; otherwise smooth the SVD estimates and interpolate
+    if(useAT) fpc[[j]] = as.matrix(svdx$v[match(time[cindex[j] : (cindex[j + 1] - 1)], utime), 1 : numk])
+    if(!useAT) fpc[[j]] = apply(as.matrix(svdx$v[, 1 : numk]), 2, function(x) {splinefun(t, x, method = 'natural') (time[cindex[j] : (cindex[j + 1] - 1)])})
+    # calculate principal scores
+    fscore[, findex[j] : (findex[j + 1] - 1)] = (crossprod(t(svdx$u), diag(svdx$d)))[(1 : numx) + (j - 1) * numx, 1 : numk]
+  }
+
+  # initial sampling variance
+  svar = rep(1, numc)
+  # initial baseline coefficients
+  zinfo = get_initial_zcoef(xobs, zobs, sbasis, svar, fscore, fpc, time)
+  zcoef = zinfo$zcoef; eta = zinfo$eta
+  # initial basis coefficients
+  sinfo = get_initial_scoef(xobs, zobs, zcoef, sbasis, penat, svar, fscore, fpc, time)
+  scoef = sinfo$scoef; lambda = sinfo$lambda; fscore = sinfo$fscore
+
+  # initialize graph information (empty)
+  gstructure = matrix(0, numc, numc); fstructure = matrix(0, numc * numk, numc * numk); mass = rep(1, numc * numk)
+  fcoef = matrix(0, numc * numk, numc * numk); fvar = matrix(1, numx, numc * numk); cvar = 1
+  cmat = matrix(1, numx, numc * numk); degree = rep(1, numc * numk); eprob = 0.5
+
+  tindex = NULL; for(j in 1 : numc) tindex = c(tindex, match(time[cindex[j] : (cindex[j + 1] - 1)], sort(unique(time))))
+  # match subscript for c++ input
+  cindex = cindex - 1; findex = findex - 1; tindex = tindex - 1
+
+  # run 10 iterations to obtain ordering
+  for(iter in 1 : 10) {
+    svar = get_svar(xobs, zobs, zcoef, scoef, sbasis, fscore, tindex, cindex, findex)
+    # baseline effects
+    zinfo = get_zcoef(xobs, zobs, scoef, sbasis, fscore, eta, svar, tindex, cindex, findex)
+    zcoef = zinfo$zcoef; eta = zinfo$eta
+    # spline information
+    sinfo = get_scoef(xobs, zobs, zcoef, scoef, sbasis, penat, fscore, svar, lambda, tindex, cindex, findex, orderLambdas = 0)
+    scoef = sinfo$scoef; lambda = sinfo$lambda
+    # principal scores
+    fscore = get_fscore(xobs, zobs, zcoef, scoef, sbasis, fvar, fcoef, svar, tindex, cindex, findex)
+    # causal structure information
+    ginfo = get_ginfo(fscore, fstructure, gstructure, fvar, cmat, degree, mass, findex, cvar, eprob)
+
+    fvar = ginfo$fvar; fcoef = ginfo$fcoef; gstructure = ginfo$gstructure; fstructure = ginfo$fstructure
+    cvar = ginfo$cvar; eprob = ginfo$eprob; cmat = ginfo$cmat; degree = ginfo$degree; mass = ginfo$mass
+  }
+
+  # order the k components based on the smoothing parameters lambda
+  sorder = order(lambda, decreasing = TRUE); lambda = lambda[sorder]; scoef = scoef[, sorder]
+  for(j in 1 : numc) {fposition = (findex[j] + 1) : findex[j + 1]; fscore[, fposition] = fscore[, fposition][, sorder]}
+
+  # make sure the l = 1 curve is positive and might as well initialize all curves to have positive sums
+  for(k in 1 : numk) {
+    if(sum(crossprod(t(sbasis), scoef[, k])) < 0) {
+      scoef[, k] = - scoef[, k]; fscore[, seq(from = k, to = numc * numk , by = numk)] = - fscore[, seq(from = k, to = numc * numk, by = numk)]
+    }
+  }
+
+  list(fscore = fscore, zcoef = zcoef, eta = eta, scoef = scoef, lambda = lambda, svar = svar, fvar = fvar, fcoef = fcoef, numk = numk, penat = penat, findex = findex,
+       gstructure = gstructure, fstructure = fstructure, cvar = cvar, eprob = eprob, cmat = cmat, degree = degree, mass = mass, sbasis = sbasis, cindex = cindex, tindex = tindex)
+}
+
+
